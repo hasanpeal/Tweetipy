b1074d3212d6041859139bb1b87aea5f8bac0b4b
Peal Hasan <pealh0320@gmail.com>
2024-07-08 21:12:52 -0400

    Vite react+typescript project initial setup

---
+++ Client/src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
+
+function App() {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
