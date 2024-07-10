0926e8e27dc5ed22d8db6c76174f17c276783611
Peal Hasan <pealh0320@gmail.com>
2024-07-10 17:50:32 -0400

    Necessary routes added

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -19,7 +19,6 @@ import {
 } from "../database/services/userServices";
 
 env.config();
-
 const app = express();
 const port = process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,14 +32,79 @@ app.use(
   })
 );
 
-app.use("/", async () => {
-  console.log("Route / has been directed");
+// POST Route for registering user
+app.post("/register", async (req, res) => {
+  console.log("Directed to POST Route -> /register");
   let connection = await db;
-  console.log(connection);
-  const user = await registerUser("Peal", "Hasan", "pealh0320@gmail.com", "245810");
-  if(user) console.log("User Added");
-  else console.log("Error adding user")
-})
+  const firstName: string = req.body.firstName;
+  const lastName: string = req.body.lastName;
+  const email: string = req.body.email;
+  const password: string = req.body.password;
+  const user = await findUser(email);
+  if (user) {
+    res.status(200).json({ code: 1, message: "User already exist" });
+  } else {
+    try {
+      await registerUser(firstName, lastName, email, password);
+    } catch (err) {
+      console.log("Error registering user on /register route");
+    }
+  }
+});
+
+// POST Route for updating profiles
+app.post("/updateProfile", async (req, res) => {
+  console.log("Directed to POST Route -> /updateProfile");
+  let connection = await db;
+  const email: string = req.body.email;
+  const profiles: [string] = req.body.profiles;
+  const user = await findUser(email);
+  if (!user) {
+    res.status(200).json({ code: 1, message: "User doesn't exist" });
+  } else {
+    try {
+      await updateProfiles(email, profiles);
+    } catch (err) {
+      console.log("Error updating profiles on /updateProfile route");
+    }
+  }
+});
+
+// POST Route for updating time
+app.post("/updateTime", async (req, res) => {
+  console.log("Directed to POST Route -> /updateTime");
+  let connection = await db;
+  const email: string = req.body.email;
+  const time: string = req.body.time;
+  const user = await findUser(email);
+  if (!user) {
+    res.status(200).json({ code: 1, message: "User doesn't exist" });
+  } else {
+    try {
+      await updateTime(email, time);
+    } catch (err) {
+      console.log("Error updating time on /updateTime route");
+    }
+  }
+});
+
+// POST Route for updating podcast file
+app.post("/updatePodcast", async (req, res) => {
+  console.log("Directed to POST Route -> /updatePodcast");
+  let connection = await db;
+  const email: string = req.body.email;
+  const podcast: string = req.body.podcast;
+  const user = await findUser(email);
+  if (!user) {
+    res.status(200).json({ code: 1, message: "User doesn't exist" });
+  } else {
+    try {
+      await updatePodcastFile(email, podcast);
+    } catch (err) {
+      console.log("Error updating podcast on /updatePodcast route");
+    }
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
