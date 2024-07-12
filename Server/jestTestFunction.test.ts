e4a2854edaba3dda2314ce218531226bec97e82f
Peal Hasan <pealh0320@gmail.com>
2024-07-11 21:52:04 -0400

    Test file updated

--- Server/jestTestFunction.test.ts
+++ Server/jestTestFunction.test.ts
@@ -2,9 +2,4 @@ import jestTest from "./database/db";
 import dotenv from "dotenv";
 dotenv.config();
 
-describe("jestTest", () => {
-  it("should return the correct number from environment variable", () => {
-    const res = jestTest;
-    expect(res).toBe("pealh0320 245810");
-  });
-});
+
